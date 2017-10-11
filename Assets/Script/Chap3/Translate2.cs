89c783c619b82fbb1d4752b6c5cad4d0c3032668
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-11 14:14:51 +0900

    Translate

---
+++ Assets/Script/Chap3/Translate2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translate2 : MonoBehaviour
+{
+    float zvalue = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zvalue += 0.1f;
+        Vector3 p = new Vector3(0, 0, zvalue);
+        transform.position = p;
+        if (zvalue > 10)
+        {
+            zvalue = 0;
+        }
+    }
+}
