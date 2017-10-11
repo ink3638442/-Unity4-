0d1048c552ad5af6f344829e692d27573c72e2c3
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-11 11:14:48 +0900

    Enum

---
+++ Assets/Enum.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enum : MonoBehaviour
+{
+	private GameObject previewText;
+
+	enum Hello {Hello, Oha, Yo};
+
+    // Use this for initialization
+    void Start()
+    {
+		this.previewText = GameObject.Find("PreviewText");
+
+		Hello h = Hello.Yo;
+		switch(h)
+		{
+			case Hello.Hello:
+			this.previewText.GetComponent<Text>().text = "ハロー";
+			break;
+
+			case Hello.Oha:
+			this.previewText.GetComponent<Text>().text = "おはー";
+			break;
+
+			case Hello.Yo:
+			this.previewText.GetComponent<Text>().text = "よー";
+			break;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
