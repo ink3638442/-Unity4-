77d2c0607e1e2c1ce11043a5db888f40a0bd2bbe
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-11 11:27:46 +0900

    Struct

---
+++ Assets/Struct.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Struct : MonoBehaviour
+{
+	struct Hello
+	{
+		public string name;
+		public string maker;
+		public int price;
+
+		public Hello(string n, string m, int p)
+		{
+			name = n;
+			maker = m;
+			price = p;
+		}
+	}
+
+	private GameObject previewText;
+
+    // Use this for initialization
+    void Start()
+    {
+		Hello h = new Hello("どら焼き", "和菓子", 125);
+		string s = h.name + ","  + h.maker + "," + h.price;
+		
+		this.previewText = GameObject.Find("PreviewText");
+		this.previewText.GetComponent<Text>().text = s;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
